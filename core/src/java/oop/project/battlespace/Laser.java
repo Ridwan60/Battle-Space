0007390d94371ba3832da3360f31a096f4912167
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2022-08-16 11:22:08 +0600

    Create Laser.java

---
+++ core/src/java/oop/project/battlespace/Laser.java
@@ -0,0 +1,44 @@
+package java.oop.project.battlespace;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+
+public class Laser extends BaseActor{
+
+    public Laser(float x, float y, Stage s) {
+        super(x, y, s);
+
+        loadTexture( "laser.png" );
+
+        addAction(Actions.delay(1));
+        addAction(Actions.after( Actions.fadeOut(0.5f) ));
+        addAction(Actions.after( Actions.removeActor() ));
+
+        setSpeed( 400 );
+        setMaxSpeed( 400 );
+        setDeceleration( 0 );
+    }
+
+    public void act( float dt ) {
+        super.act(dt);
+        applyPhysics( dt );
+        wrapAroundWorld();
+    }
+
+    @Override
+    public void wrapAroundWorld() {
+        if( getX() + getWidth() < 0 ) {
+            addAction( Actions.removeActor() );
+        }
+        if( getX() > Gdx.graphics.getWidth() ) {
+            addAction( Actions.removeActor() );
+        }
+        if( getY() + getHeight() < 0 ) {
+            addAction( Actions.removeActor() );
+        }
+        if( getY() > Gdx.graphics.getHeight() ) {
+            addAction( Actions.removeActor() );
+        }
+    }
+}
